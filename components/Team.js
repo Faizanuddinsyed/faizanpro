2170ef29c822041b48ed5f5ca31ec8bb4340b32e
Faizan <syedf928@gmail.com>
2023-07-08 16:12:20 +0530

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   components/Team.js

--- components/Team.js
+++ components/Team.js
@@ -12,7 +12,7 @@ function Team() {
         </div>
 
         <div className="md:ml-0 md:mb-56 md:w-[447px] md:h-[168px] w-[300px]  mx-auto">
-            <h1 className="md:mb-6 font-bison md:text-[42px] text-[30px] leading-[35px] w-[300px] h-[84px] md:text-start md:font-bold font-semibold md:leading-[45px] tracking-tighter text-center">BE A PART OF<br/><span className="whitespace-nowrap"> WELL COMMUNITY</span></h1>
+            <h1 className="md:mb-6 font-bison md:text-[42px] text-[30px] leading-[35px] w-[300px] h-[84px] md:text-start md:font-bold font-semibold md:leading-[45px] tracking-tighter text-center">BE A PART OF<br/><span className="whitespace-nowrap"> WELLNESS COMMUNITY</span></h1>
             <p className="font-roboto md:mt-3 font-400 md:text-start w-[300px] h-[160px] md:w-[447px] md:h-[168px] text-center text-[14px] md:text-[18px]">The Heartfulness International Sports Center (HISC) was conceived in 2020 as an integrated multi-sport facility to promote excellence in sports, with the guidance of leading sports icons such as <span  className="font-bold">Pullela Gopichand, P. V. Sindhu,</span> and with a close association of sporting inspirations like that of <span className="font-bold">Havier Zanett, Rohit Sharma, R. Ashwin, and Ravi Shashtri.</span></p>
             <h1 className="text-center md:mt-12 mt-8 md:mb-0 mb-8   ml-16 md:ml-0 rounded-3xl p-5   text-white bg-orange-600 md:bg-blue-600  w-[175px] h-[52px] font-roboto font-medium text-xs">SIGN UP NOW</h1>
         </div>
